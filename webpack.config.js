f7c7a1ec43f64c36844d5606a32fb3c85648fa5f
Bhairesh M <bhairesh@arithaconsulting.com>
2023-03-13 22:12:16 +0530

    1.0.11

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     target: "node",
-    entry: 'app.js',
+    entry: './app.js',
     mode: "production",
     devtool: 'inline-source-map',
     devServer: {
