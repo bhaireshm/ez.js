a7b96e64ab0628e8e195e4f122f24d596ee80d6f
Bhairesh M <bhairesh@arithaconsulting.com>
2023-04-19 23:01:49 +0530

    update: getNestedKeyValue method added.

---
+++ src/helpers/getNestedKeyValue.js
@@ -0,0 +1,26 @@
+
+/**
+ * Get nested value from the given object.
+ * 
+ * @param {object} d
+ * @param {string} k - key name separated by dot character
+ * @example 
+ * const data = {
+    pid: 'some-id',
+    portions: { name: 'section' }
+}
+ * console.log(getNestedKeyValue(data, "portions.name")); // 'section'
+ * 
+ * @returns value, If nothing found null will be returned.
+ */
+function getNestedKeyValue(d = {}, k = "") {
+    const keys = String(k).split(".");
+    if (!keys.length) return null;
+    return keys.reduce((p, c) => Object(p).hasOwnProperty(c) ? p[c] : null, d);
+}
+
+const data = {
+    pid: 'some-id',
+    portions: { name: 'section' }
+}
+console.log(getNestedKeyValue(data, "portions.nameas")); // 'section'
