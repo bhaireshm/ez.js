bd7c220dd48fb813e10595197870b9a280da0cad
Bhairesh M <bhairesh@arithaconsulting.com>
2023-08-08 12:40:13 +0530

    configs: release script updated.

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -9,57 +9,51 @@
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Stop on first error
-set -e;
+set -e
 
 # Ensure that the provided version is in valid semver format
 if [[ ! $1 =~ ^v?[0-9]+(\.[0-9]+){2}(-[a-z]+\.\d+)?$ ]]; then
-  echo "A valid version must be provided as the first argument.";
-  exit 1;
+  echo "A valid version must be provided as the first argument."
+  exit 1
 fi
 
-ver=${1/v/}; # Strip the leading v from the version (if it exists)
-msg=$2;
+ver=${1/v/} # Strip the leading v from the version (if it exists)
+msg=$2
 
-[[ -z $msg ]] && msg="Released v${ver}";
+[[ -z $msg ]] && msg="Released v${ver}"
 
 # Update the master branch to the latest
-git checkout master;
-git pull origin master;
+git checkout master
 
 # Update develop to the latest, and create a release brach off of it.
 # git checkout develop;
 # git pull origin develop;
-git checkout -b release/$ver;
+git checkout -b release/$ver --no-verify
 
 # Bump version in package.json, but do not create a git tag
-pnpm version $ver --no-git-tag-version;
+pnpm version $ver --no-git-tag-version
 
 # Inject the current release version and date into the CHANGELOG file
 # If changelog.md exists add updated version
 
 # Find all commits between the HEAD on develop and the latest tag on master, and pipe their messages into the clipboard
-# git log $(git describe --tags master --abbrev=0)..HEAD --merges --pretty=format:'* %s' | pbcopy;
+git log $(git describe --tags master --abbrev=0)..HEAD --merges --pretty=format:'* %s' | pbcopy
 
 # Provision manual intervention for CHANGELOG.md
 vi CHANGELOG.md
 
 # Create the release
-# git add CHANGELOG.md package.json;
-[[ -f package-lock.json ]] && git add package-lock.json;
-git commit -am "$msg";
-
-# Merge the release branch into develop and push
-# git checkout develop;
-# git merge --no-ff release/$ver;
-# git push origin develop;
+git add CHANGELOG.md package.json
+[[ -f package-lock.json ]] && git add package-lock.json
+git commit -am "$msg"
 
 # Merge the release branch into master, create a tag and push
-git checkout master;
-git merge --no-ff release/$ver; # fast-forward
-git tag -a "$ver" -m "$msg";
-git push origin master --follow-tags;
+git checkout master
+git merge --no-ff release/$ver # fast-forward
+git tag -a "$ver" -m "$msg"
+git push origin master --follow-tags
 
-git branch -d release/$ver;
+git branch -d release/$ver
 
 # clear variables
-unset msg ver;
+unset msg ver
