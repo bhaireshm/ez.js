1e346079f273f328dcae44b2aa152c1700f19fdc
Bhairesh M <bhairesh@arithaconsulting.com>
2023-08-11 17:02:52 +0530

    update: release script updated.

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -21,7 +21,7 @@ fi
 ver=${1/v/}
 msg=$2
 
-[[ -z $msg ]] && msg="Released"
+# [[ -z $msg ]] && msg="Released"
 
 # Update the master branch to the latest
 git checkout master
@@ -38,23 +38,19 @@ changelog="## $ver: $msg"
 
 # Find all commits between the HEAD on master and the latest tag on master, and pipe their messages into the clipboard
 # commits=$(git log $(git describe --tags master --abbrev=0)..HEAD --merges --pretty=format:'* %s')
-commits=$(git log --pretty=format:"(%h) %s %d [%cn]%n" --grep="fix:\|feat:\|update:")
+commits=$(git log $(git describe --tags master --abbrev=0)..HEAD --pretty=format:"(%h) %s %d [%cn]%n" --grep="fix:\|feat:\|update:")
 
-cat >$changelog <<X
-$changelog
-$commits
-X
-
-echo "$changelog" | cat - CHANGELOG.md >temp && mv temp CHANGELOG.md
+changelog="$changelog\n$msg\n$commits\n"
+echo -e "$changelog" | cat - CHANGELOG.md >temp.md && mv temp.md CHANGELOG.md
 
 # Create the release
 git add CHANGELOG.md package.json
-git commit -am "$msg"
+git commit -am "release: v$ver"
 
 # Merge the release branch into master, create a tag and push
 git checkout master
 git merge --no-ff release/$ver # fast-forward
-git tag -a "$ver" -m "$msg"
+git tag -a "v$ver" -m "release-v$ver"
 git push origin master --follow-tags
 
 git branch -d release/$ver
