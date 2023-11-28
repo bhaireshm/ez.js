91e63f0078bd9c7efc7fe2a2bda250d48ceb084f
Bhairesh M <bhairesh@arithaconsulting.com>
2023-11-28 22:12:49 +0530

    config: jsDoc to typeDoc.

--- src/scripts/shell/index.sh
+++ src/scripts/shell/index.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 check_semver() {
-    # Define the regular expression for valid semantic version
-    semver_pattern="^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?(\+([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?$"
+  # Define the regular expression for valid semantic version
+  semver_pattern="^([0-9]\d*)\.([0-9]\d*)\.([0-9]\d*)(-([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?(\+([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?$"
 
-    # Check if the provided string matches the pattern
-    if [[ $1 =~ $semver_pattern ]]; then
-        echo "$1 is a valid semantic version."
-    else
-        echo "$1 is not a valid semantic version."
-    fi
+  # Check if the provided string matches the pattern
+  if [[ $1 =~ $semver_pattern ]]; then
+    echo "$1 is a valid semantic version."
+  else
+    echo "$1 is not a valid semantic version."
+  fi
 }
 
 # Test the script with the provided versions
-versions=("0.0.4" "1.2.3-rc.1" "10.20.30-rc1" "1.1.2-prerelease+meta" "1.0.0-alpha" "1" "alpha" "alpha." "-alpha.")
+# versions=("0.0.4" "1.2.3-rc.1" "10.20.30-rc1" "1.1.2-prerelease+meta" "1.0.0-alpha" "1" "alpha" "alpha." "-alpha.")
 
-for version in "${versions[@]}"; do
-    check_semver "$version"
-done
+# for version in "${versions[@]}"; do
+check_semver "$1"
+# done
