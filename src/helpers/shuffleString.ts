90df8fbc9d1c769db2d682aee2fbadc731689802
Bhairesh M <bhairesh@arithaconsulting.com>
2023-11-30 15:04:50 +0530

    update: shuffleString. - converted to typescript. - test cases added.

---
+++ src/helpers/shuffleString.ts
@@ -0,0 +1,21 @@
+import isStr from "../helpers/isStr";
+
+/**
+ * Shuffles the characters of a given string and returns the shuffled string.
+ *
+ * @param {string} str - The input string to be shuffled.
+ * @returns {string} The shuffled string.
+ */
+export default function shuffleString(str: string): string {
+  str = str.trim().replace(/ /g, "");
+
+  if (!isStr(str)) return str;
+  const array = str.split("");
+
+  // Fisher-Yates Shuffle Algorithm
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array.join("");
+}
