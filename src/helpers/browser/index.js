d78638f2867a4734fb5653928cf391292e018a24
Bhairesh M <bhairesh@arithaconsulting.com>
2023-11-30 10:23:40 +0530

    update: objectToQueryParams. - converted to typescript. - test cases added.

---
+++ src/helpers/browser/index.js
@@ -0,0 +1,2 @@
+export { default as blockBrowserBackButton } from "./blockBrowserBackButton.js";
+export { default as createTable } from "./createTable.js";
--- src/helpers/objectToQueryParams.ts
+++ src/helpers/objectToQueryParams.ts
@@ -4,10 +4,8 @@
  * @param {object} o - The object to be converted into query parameters.
  * @returns {string} - The query parameters string.
  */
-function objectToQueryParams(o = {}) {
+export default function objectToQueryParams(o: { [key: string]: any } = {}): string {
   return Object.entries(o)
     .map((p) => `${encodeURIComponent(p[0])}=${encodeURIComponent(p[1])}`)
     .join("&");
 }
-
-module.exports = objectToQueryParams;
