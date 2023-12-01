3e292bffaabddd201eb42755961069efe0d83ede
Bhairesh M <bhairesh@arithaconsulting.com>
2023-12-01 14:28:05 +0530

    chore: case method rename.

--- src/helpers/toCamelCase.ts
+++ src/helpers/toCamelCase.ts
@@ -4,6 +4,6 @@
  * @param {string} str - The input string that needs to be converted to camel case.
  * @returns {string} - Returns a new string where each word's first letter is converted to uppercase.
  */
-export default function camelCase(str: string): string {
+export default function toCamelCase(str: string): string {
   return str.trim().replace(/[^a-zA-Z0-9]+(\w|$)/g, (_, p1) => p1.toUpperCase());
 }
