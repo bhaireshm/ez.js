e18237756b4628c48a8dd45e7e6f1fa780d15578
Bhairesh M <bhairesh@arithaconsulting.com>
2024-08-28 15:26:22 +0530

    docs: mergeObjects docstring updated.

--- src/helpers/mergeObjects.ts
+++ src/helpers/mergeObjects.ts
@@ -5,27 +5,46 @@ import isObj from "./isObj";
 /**
  * Uses deep merge way to merge objects.
  *
- * @param {object} obj1 - The first object to be merged.
- * @param {object} obj2 - The second object to be merged.
- * @returns {object} - The merged object containing the combined properties of obj1 and obj2.
+ * @param {object} source - The first object to be merged.
+ * @param {object} target - The second object to be merged.
+ * @returns {object} - The merged object containing the combined properties of source and target.
+ *
+ * @example
+ * const obj1 = { a: 1, b: 2 };
+ * const obj2 = { c: 3, d: 4 };
+ * const result = mergeObjects(obj1, obj2);
+ * console.log(result); // { a: 1, b: 2, c: 3, d: 4 }
+ *
+ * @example
+ * const obj1 = { a: 1, b: [1, 2] };
+ * const obj2 = { b: [3, 4], c: 4 };
+ * const result = mergeObjects(obj1, obj2);
+ * console.log(result); // { a: 1, b: [1, 2, 3, 4], c: 4 }
+ *
+ * @example
+ * const obj1 = { a: { b: 1, c: [1, 2] } };
+ * const obj2 = { a: { c: [3, 4], d: 4 }, e: 5 };
+ * const result = mergeObjects(obj1, obj2);
+ * console.log(result); // { a: { b: 1, c: [1, 2, 3, 4], d: 4 }, e: 5 }
  */
-export default function mergeObjects(obj1: AnyObject, obj2: AnyObject): AnyObject {
-  const o1 = { ...obj1 };
-  const o2 = { ...obj2 };
+export default function mergeObjects(source: AnyObject, target: AnyObject): AnyObject {
+  const s = { ...source };
+  const t = { ...target };
 
-  for (const key in o1) {
-    if (hasOwnProperty(o1, key, true)) {
-      if (Array.isArray(o2[key]) && Array.isArray(o1[key])) {
-        o2[key] = [...o1[key], ...o2[key]];
+  for (const key in s) {
+    if (hasOwnProperty(s, key, true)) {
+      if (Array.isArray(t[key]) && Array.isArray(s[key])) {
+        t[key] = [...s[key], ...t[key]];
         continue;
       }
-      if (isObj(o2[key]) && isObj(o1[key])) {
-        o2[key] = mergeObjects(o1[key], o2[key]);
+      if (isObj(t[key]) && isObj(s[key])) {
+        t[key] = mergeObjects(s[key], t[key]);
         continue;
       }
-      o2[key] = o1[key];
+      t[key] = s[key];
     }
   }
 
-  return o2;
+  return t;
 }
+
